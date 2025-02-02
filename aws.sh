f8ffda611c944822978267e5f70511f15cbe45c6
Laxmansandy1998 <slaxmans0007@gmail.com>
2025-02-02 20:52:08 +0000

    first

---
+++ aws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+######################################
+# Author:Laxman
+# version : v0.0.1
+# descpretion : list aws resource
+# needs : aws cli
+
+
+echo "ec2 list"
+aws ec2 describe-instances
